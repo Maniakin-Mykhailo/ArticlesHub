486e797947eb83995405657275329cfaea8da344
Cremson <89212136+Maniakin-Mykhailo@users.noreply.github.com>
2023-06-25 23:51:05 +0300

    Add project files.

---
+++ ArticlesHub/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ArticlesHub.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticlesHub.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
