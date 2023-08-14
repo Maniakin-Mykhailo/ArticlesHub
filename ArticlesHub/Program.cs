faa9f307c41d5bae0b503257832603f953caea46
Cremson <89212136+Maniakin-Mykhailo@users.noreply.github.com>
2023-08-14 03:00:07 +0300

    middleware changes

--- ArticlesHub/Program.cs
+++ ArticlesHub/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 
